6450e25d7ba141db08f68f1b00e738a4d5ebf662
tonguiino <santiagotq2@gmail.com>
2024-05-23 16:43:11 -0500

    Create skills component and add styles

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,6 +1,19 @@
-const Skills=()=>{
-    return(
-<h2>Habilidades</h2>
+import "./Skills.scss"
+
+const Skills = () => {
+    return (
+        <section className="skills">
+            <div>
+                <h2>Habilidades</h2>
+                <p>
+                    A continuacion podran ver las tecnologias que actualmente se utilizar y estoy aprendiendo
+                </p>
+            </div>
+            <div className="skills-box">
+                
+            </div>
+        </section>
+
     )
 }
 export default Skills
