d6c0fb4fb766eeb6a192ddf8b0d3d849a26aeba9
tonguiino <santiagotq2@gmail.com>
2024-06-05 10:23:37 -0500

    Fix some mistakes in the code in contac for camelcase

--- src/Components/Contac/Contac.jsx
+++ src/Components/Contac/Contac.jsx
@@ -9,7 +9,7 @@ const Contac = () => {
                     </p>
                 </div>
                 <div className="contac-nav">
-                    <button class="button">
+                    <button className="button">
                         <a href="https://www.instagram.com/tonguiino/" target="_blank">
                             <svg
                                 viewBox="0 0 24 24"
@@ -18,18 +18,18 @@ const Contac = () => {
                                 width="24"
                                 xmlns="http://www.w3.org/2000/svg"
                                 aria-hidden="true"
-                                class="w-6 h-6 text-gray-800 dark:text-white"
+                                className="w-6 h-6 text-gray-800 dark:text-white"
                             >
                                 <path
-                                    clip-rule="evenodd"
+                                    clipRule="evenodd"
                                     d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"
-                                    fill-rule="evenodd"
+                                    fillRule="evenodd"
                                     fill="currentColor"
                                 ></path>
                             </svg>
                         </a>
                     </button>
-                    <button class="button">
+                    <button className="button">
                         <a href="https://api.whatsapp.com/send/?phone=%2B573008166510&text&type=phone_number&app_absent=0" target="_blank">
                             <svg
                                 viewBox="0 0 24 24"
@@ -38,12 +38,12 @@ const Contac = () => {
                                 width="24"
                                 xmlns="http://www.w3.org/2000/svg"
                                 aria-hidden="true"
-                                class="w-6 h-6 text-gray-800 dark:text-white"
+                                className="w-6 h-6 text-gray-800 dark:text-white"
                             >
                                 <path
-                                    clip-rule="evenodd"
+                                    clipRule="evenodd"
                                     d="M12 4a8 8 0 0 0-6.895 12.06l.569.718-.697 2.359 2.32-.648.379.243A8 8 0 1 0 12 4ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.96 9.96 0 0 1-5.016-1.347l-4.948 1.382 1.426-4.829-.006-.007-.033-.055A9.958 9.958 0 0 1 2 12Z"
-                                    fill-rule="evenodd"
+                                    fillRule="evenodd"
                                     fill="currentColor"
                                 ></path>
                                 <path
@@ -53,7 +53,7 @@ const Contac = () => {
                             </svg>
                         </a>
                     </button>
-                    <button class="button">
+                    <button className="button">
                         <a href="https://github.com/tonguiino" target="_blank">
                             <svg
                                 viewBox="0 0 24 24"
@@ -62,30 +62,32 @@ const Contac = () => {
                                 width="24"
                                 xmlns="http://www.w3.org/2000/svg"
                                 aria-hidden="true"
-                                class="w-6 h-6 text-gray-800 dark:text-white"
+                                className="w-6 h-6 text-gray-800 dark:text-white"
                             >
                                 <path
-                                    clip-rule="evenodd"
+                                    clipRule="evenodd"
                                     d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z"
-                                    fill-rule="evenodd"
+                                    fillRule="evenodd"
                                 ></path>
                             </svg>
                         </a>
                     </button>
                 </div>
             </div>
-            <div class="terminal-loader">
-                <div class="terminal-header">
-                    <div class="terminal-title">Status</div>
-                    <div class="terminal-controls">
-                        <div class="control close"></div>
-                        <div class="control minimize"></div>
-                        <div class="control maximize"></div>
+            <div className="contac-terminal-loader-container">
+            <div className="terminal-loader">
+                <div className="terminal-header">
+                    <div className="terminal-title">Status</div>
+                    <div className="terminal-controls">
+                        <div className="control close"></div>
+                        <div className="control minimize"></div>
+                        <div className="control maximize"></div>
                     </div>
                 </div>
-                <div class="content">
-                    <div class="text">Disponible para trabajar...</div>
+                <div className="content">
+                    <div className="text">Disponible para trabajar...</div>
                 </div>
+            </div>
             </div>
 
         </section>
