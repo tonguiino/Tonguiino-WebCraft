952febbe8e5fdd8b5e97b1b0d91b02ab8275666d
tonguiino <santiagotq2@gmail.com>
2024-07-11 16:07:44 -0500

    Add and fix styles home, add styling first mobile

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -11,21 +11,26 @@ const Button = ({ text }) => {
 const Home = () => {
     return (
         <section className="home" id="home">
+            <div className="profile-img">
+                <div class="card">
+                    <img src={profileImage} alt="" />
+                </div>
+
+            </div>
             <div className="profile-info">
                 <h1>Desarrollador Web</h1>
                 <h2>Ey, soy Santiago Tonguino</h2>
                 <p>Me dedico al desarrollo web, enfocándome en construir sitios web atractivos y totalmente funcionales.
                     Mi dominio abarca las tecnologías esenciales como HTML5, CSS, JavaScript, React, Sass, WordlPress,
                     PHP y SQL server.</p>
-                {/* <div className="prodfile-info-button"> */}
-                <Button text={'Descargar cv'} />
-                <Button text={<a href="https://www.linkedin.com/in/santiago-tonguino-quiceno-41a44b167/" target="_blank" >LinkedIn</a>} />
-                {/* </div> */}
-            </div>
-            <div className="profile-img">
-                <img src={profileImage} alt="" />
+                <div className="prodfile-info-button">
+                    <Button text={'Descargar cv'} />
+                    <Button text={<a href="https://www.linkedin.com/in/santiago-tonguino-quiceno-41a44b167/" target="_blank" >LinkedIn</a>} />
+                </div>
             </div>
         </section>
+
+
 
 
     )
