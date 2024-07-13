00021fc23ddc8c4a20251e8f38844c8be6c0878c
tonguiino <santiagotq2@gmail.com>
2024-07-12 19:29:40 -0500

    fix styles home and skills

--- src/Components/Contac/Contac.jsx
+++ src/Components/Contac/Contac.jsx
@@ -75,19 +75,19 @@ const Contac = () => {
                 </div>
             </div>
             <div className="contac-terminal-loader-container">
-            <div className="terminal-loader">
-                <div className="terminal-header">
-                    <div className="terminal-title">Status</div>
-                    <div className="terminal-controls">
-                        <div className="control close"></div>
-                        <div className="control minimize"></div>
-                        <div className="control maximize"></div>
+                <div className="terminal-loader">
+                    <div className="terminal-header">
+                        <div className="terminal-title">Status</div>
+                        <div className="terminal-controls">
+                            <div className="control close"></div>
+                            <div className="control minimize"></div>
+                            <div className="control maximize"></div>
+                        </div>
+                    </div>
+                    <div className="content">
+                        <div className="text">Disponible para trabajar...</div>
                     </div>
                 </div>
-                <div className="content">
-                    <div className="text">Disponible para trabajar...</div>
-                </div>
-            </div>
             </div>
 
         </section>
