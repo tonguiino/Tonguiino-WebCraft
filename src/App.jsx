f95492b176008ec6e8c8c7d23277e93a3f15029d
tonguiino <santiagotq2@gmail.com>
2025-01-21 12:32:50 -0500

    Add Preload page and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,48 @@
-import { useState } from 'react'
-import './App.scss'
-import Header from './Components/Header/Header'
-import Home from './Components/Home/Home'
-import AboutMe from './Components/AboutMe/AboutMe'
-import Contac from './Components/Contac/Contac'
-import Skills from './Components/Skills/Skills'
-import Footer from './Components/Footer/Footer'
-
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import AboutMe from "./Components/AboutMe/AboutMe";
+import Contac from "./Components/Contac/Contac";
+import Skills from "./Components/Skills/Skills";
+import Footer from "./Components/Footer/Footer";
+import Loader from "./Components/Loader/Loader";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
+      {isLoading ? (
 
-      <Header />
-      <main>
-        <Home />
-        <Skills />
-        <AboutMe />
-        <Contac />
-      </main>
-      <Footer />
+        <Loader />
+      ) : (
+
+        <>
+          <Header />
+          <main>
+            <Home />
+            <Skills />
+            <AboutMe />
+            <Contac />
+          </main>
+          <Footer />
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,14 @@
+import "./Loader.scss";
+const Loader = () => {
+    return (
+
+        <section className="loader">
+            <div className="loader-container">
+                <div className="loader-small"></div>
+                <div className="loader-large"></div>
+            </div>
+        </section>
+
+    )
+};
+export default Loader
