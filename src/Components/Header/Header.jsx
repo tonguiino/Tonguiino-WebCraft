c580b340bf595c9ed121db40a3443e59d3984e56
tonguiino <santiagotq2@gmail.com>
2025-06-13 12:15:57 -0500

    Se implementa funcion mobil para ocultar menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react"
 import "./Header.scss"
 import ItemsNav from "./ItemsNav.json"
 
 const Header = () => {
+
+    const [isMenu, setIsMenu] = useState(false)
+    const handleMenu = () => setIsMenu(!isMenu)
     return (
         <header>
             <nav>
-                <ul>
+                <div className="btn-menu" onClick={handleMenu}>
+                    {isMenu ? <span class="material-symbols-outlined">close</span> :
+                        <span className="material-symbols-outlined">menu</span>}
+                </div>
+                {isMenu && (<ul>
                     {ItemsNav.map((item, index) => {
                         return (
                             <li className="list-item" key={index}><a href={item.path}>{item.name}</a></li>
                         )
                     })}
-                </ul>
+                </ul>)}
+
             </nav>
         </header>
 
