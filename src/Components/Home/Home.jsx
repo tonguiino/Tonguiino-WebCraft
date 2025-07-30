82aea8e2cae790bc08981540ccd7ca148010fa03
tonguiino <santiagotq2@gmail.com>
2025-07-29 19:57:00 -0500

    feat: integrate GSAP for animations on Home component
    - Added GSAP and @gsap/react dependencies to package.json.
    - Implemented GSAP animations in Home.jsx for the home section with ScrollTrigger.
    - Updated Home.scss to include styles for animated elements.
    - Adjusted index.scss for responsive design improvements.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,9 @@
 import "./Home.scss"
 import profileImage from "../../assets/img/FotoPerfilSantiagoTonguino.jpg"
-
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Button = ({ text }) => {
     return (
@@ -9,11 +12,30 @@ const Button = ({ text }) => {
 }
 
 const Home = () => {
+    const homeRef = useRef();
+
+    // Efectos usados con gsap 
+    useGSAP(() => {
+        gsap.from(homeRef.current, {
+            Y: 100, //desplazamiento
+            opacity: 0, //opacidad
+            duration: 4, //duracion
+            ease: "bounce", //efecto 
+            ScrollTrigger: {
+                trigger: homeRef.current,
+                start: "top 80%", // Cuando el top del elemento llegue al 80% del viewport
+                toggleActions: "play none none none" // play cuando entra
+
+            }
+        });
+    }, []);
+
+
     return (
-        <section className="home" id="home">
+        <section className="home" id="home" ref={homeRef}>
             <div className="profile-img">
                 <div className="card">
-                    <img src={profileImage} alt="Fotografia de perfil Santiago Tonguino" title="Profile img"/>
+                    <img src={profileImage} alt="Fotografia de perfil Santiago Tonguino" title="Profile img" />
                 </div>
 
             </div>
@@ -23,7 +45,7 @@ const Home = () => {
                 <p>Me dedico al desarrollo web, enfocándome en construir sitios web atractivos y totalmente funcionales.
                     Mi dominio abarca las tecnologías esenciales como HTML5, CSS, JavaScript, React, Sass, WordlPress,
                     PHP y SQL server.</p>
-                <div className="prodfile-info-button">
+                <div className="prodfile-info-button animado">
                     <Button text={<a href="https://drive.google.com/file/d/1YbLPll2FqawjixDK6CVL53jcUcTSNcJY/view" target="_blank">Visita mi cv</a>} />
                     <Button text={<a href="https://www.linkedin.com/in/santiago-tonguino-quiceno-41a44b167/" target="_blank" >LinkedIn</a>} />
                 </div>
