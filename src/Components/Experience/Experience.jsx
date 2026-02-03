2d7b6041e286941e2d816f0c102d83e0939b6542
tonguiino <santiagotq2@gmail.com>
2026-02-03 16:16:00 -0500

    Se crea componernte grid con primera info de exp laboral

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,14 +1,33 @@
 import { useRevealOnScroll } from "../../hooks/useRevealOnScroll"
 import { useRef } from 'react'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
 
-const Experience =()=>{
+
+const Experience = () => {
 
     const experienceRef = useRef()
     useRevealOnScroll(experienceRef)
 
-    return(
+    return (
         <section ref={experienceRef} >
-            holaaa
+            <Box>
+                <Grid container spacing={2}>
+                    <Grid size={8}>
+                        <Box sx={{ height: '100%', boxSizing: 'border-box' }}>
+                            <h2>Experiencia laboral</h2>
+                            <p>Estos son algunos lugares en donde he aportado valor como Desarrollador profesional</p>
+                        </Box>
+                    </Grid>
+                    <Grid size={4}>
+                        <Stack spacing={2}>
+                            <Box>Column 1 - Row 1</Box>
+                            <Box>Column 1 - Row 2</Box>
+                        </Stack>
+                    </Grid>
+                </Grid>
+            </Box>
         </section>
     )
 }
