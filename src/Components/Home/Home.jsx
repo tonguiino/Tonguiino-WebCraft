9c210ed029dd10ac8b47c736032c5a9783990456
tonguiino <santiagotq2@gmail.com>
2026-04-24 17:04:06 -0500

    feat: add Home component with GSAP animations and CV download functionality

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,6 +3,7 @@ import profileImage from "../../assets/img/FotoPerfilSantiagoTonguino.jpg"
 import { gsap } from "gsap";
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
+import cvSantiago from "../../assets/pdf/cv_santiago_tonguino.pdf"
 
 
 const Home = () => {
@@ -38,6 +39,14 @@ const Home = () => {
         });
     }, { scope: homeRef });
 
+    const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = cvSantiago;
+    link.download = 'CV_Santiago_Tonguino.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
     return (
         <section className="home" id="home" ref={homeRef}>
@@ -52,7 +61,7 @@ const Home = () => {
                 <h2>Ey, soy Santiago Tonguino</h2>
                 <p>Me dedico al desarrollo web full-stack, creando sitios atractivos, funcionales y escalables. Domino tecnologías frontend (HTML5, CSS, JavaScript, TypeScript, React, Next.js, Sass), backend (PHP, Laravel, WordPress, SQL Server), y DevOps (Docker, Caddy, Nginx) para entregar soluciones completas y optimizadas.</p>
                 <div className="prodfile-info-button animado">
-                    <button onClick={() => window.open('https://drive.google.com/file/d/1YbLPll2FqawjixDK6CVL53jcUcTSNcJY/view', '_blank')} >Conoce mi CV</button>
+                   <button onClick={handleDownload} >Conoce mi CV</button>
                     <button onClick={() => window.open('https://www.linkedin.com/in/tonguiino/', '_blank')} >linkedin</button>
                 </div>
             </div>
